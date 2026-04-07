f976c67946fd148f74e016647bbb932fc555d0c3
Nguyễn Trương Quý Dương <nduongg110@gmail.com>
2026-04-07 19:00:37 +0700

    daily update

--- src/components/HeaderComponent/HeaderComponent.jsx
+++ src/components/HeaderComponent/HeaderComponent.jsx
@@ -1,15 +1,43 @@
 import { Col } from 'antd'
 import React from 'react'
-import { WrapperHeader } from './style'
+import { WrapperHeader, WrapperTextHeader, WarpHeaderAccount, WrapperTextHeaderSmall } from './style'
+import Search from 'antd/es/transfer/search'
+import { UserOutlined, CaretDownOutlined, ShoppingCartOutlined } from '@ant-design/icons'
 
 const HeaderComponent = () => {
   return (
     <div>
-      <WrapperHeader>
-        <Col span={6}>col-6</Col>
-        <Col span={12}>col-12</Col>
-        <Col span={6}>col-6</Col>
-      </WrapperHeader>
+      <WrapperHeader gutter={16}>
+        <Col span={6}>
+          <WrapperTextHeader>QDPHONE</WrapperTextHeader>
+        </Col>
+        <Col span={12}>
+        <Search
+          placeholder="input search text"
+          allowClear
+          enterButton="Search"
+          size="large"
+          //OnSearch={onSearch}
+        />
+
+        </Col>
+        <Col span={6} style = {{display: 'flex', gap: '20px'}}>
+          <WarpHeaderAccount>
+            <UserOutlined style={{ fontSize: '30px' }} />
+          <div>
+            <WrapperTextHeaderSmall style={{ fontSize: '12px' }}>Đăng nhập/Đăng ký</WrapperTextHeaderSmall>
+          <div>
+              <WrapperTextHeaderSmall style={{ fontSize: '12px' }}>Tài khoản</WrapperTextHeaderSmall>
+              <CaretDownOutlined />
+          </div>
+          </div>
+          </WarpHeaderAccount>
+          <div>
+              <ShoppingCartOutlined style={{ fontSize: '30px', color: '#fff' }} />
+              <WrapperTextHeaderSmall>Giỏ hàng</WrapperTextHeaderSmall>
+          </div>
+        </Col>
+        </WrapperHeader>
     </div>
   )
 }
--- src/components/HeaderComponent/style.js
+++ src/components/HeaderComponent/style.js
@@ -3,5 +3,26 @@ import styled from "styled-components";
 
 export const WrapperHeader = styled(Row)`
   padding: 10px 120px;
-  background-color: rgb(26, 148, 255);
-`;
+  background-color: rgb(0, 100, 0);
+  align-items: center;
+`
+
+export const WrapperTextHeader = styled.span`
+  font-size: 18px;
+  color: #fff;
+  font-weight: bold;
+  text-align: left;
+`
+export const WarpHeaderAccount = styled.div`
+  display: flex;
+  align-items: center;
+  color: #fff;
+  gap: 10px;
+`
+export const WrapperTextHeaderSmall = styled.span`
+    font-size: 12px;
+    color: #fff;
+`
+
+
+
