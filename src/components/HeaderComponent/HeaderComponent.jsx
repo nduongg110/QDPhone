acb2af30ab6b3a9ad702138d8d38f2af5b3a3832
Nguyễn Trương Quý Dương <nduongg110@gmail.com>
2026-04-07 13:39:26 +0700

    daily update

--- src/components/HeaderComponent/HeaderComponent.jsx
+++ src/components/HeaderComponent/HeaderComponent.jsx
@@ -1,9 +1,15 @@
+import { Col } from 'antd'
 import React from 'react'
+import { WrapperHeader } from './style'
 
 const HeaderComponent = () => {
   return (
     <div>
-      Header
+      <WrapperHeader>
+        <Col span={6}>col-6</Col>
+        <Col span={12}>col-12</Col>
+        <Col span={6}>col-6</Col>
+      </WrapperHeader>
     </div>
   )
 }
